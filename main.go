221fca7ee00811db2039e267603d5039bfe4a4d0
二丫讲梵 <eryajf@163.com>
2023-03-15 20:11:42 +0800

    feat: 支持请求失败之后自动重试的能力 (#94)

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/avast/retry-go"
 	"github.com/eryajf/chatgpt-dingtalk/public"
 	"github.com/eryajf/chatgpt-dingtalk/public/logger"
 	"github.com/solywsh/chatgpt"
@@ -214,17 +215,46 @@ func Do(mode string, rmsg public.ReceiveMsg) error {
 func SingleQa(question, userId string) (answer string, err error) {
 	chat := chatgpt.New(userId)
 	defer chat.Close()
-	return chat.ChatWithContext(question)
+	// 定义一个重试策略
+	retryStrategy := []retry.Option{
+		retry.Delay(100 * time.Millisecond),
+		retry.Attempts(3),
+		retry.LastErrorOnly(true),
+	}
+	// 使用重试策略进行重试
+	err = retry.Do(
+		func() error {
+			answer, err = chat.ChatWithContext(question)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+		retryStrategy...)
+	return
 }
 
 func ContextQa(question, userId string) (chat *chatgpt.ChatGPT, answer string, err error) {
 	chat = chatgpt.New(userId)
 	if public.UserService.GetUserSessionContext(userId) != "" {
-		err = chat.ChatContext.LoadConversation(userId)
+		err := chat.ChatContext.LoadConversation(userId)
 		if err != nil {
-			fmt.Printf("load station failed: %v\n", err)
+			logger.Warning("load station failed: %v\n", err)
 		}
 	}
-	answer, err = chat.ChatWithContext(question)
+	retryStrategy := []retry.Option{
+		retry.Delay(100 * time.Millisecond),
+		retry.Attempts(3),
+		retry.LastErrorOnly(true)}
+	// 使用重试策略进行重试
+	err = retry.Do(
+		func() error {
+			answer, err = chat.ChatWithContext(question)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+		retryStrategy...)
 	return
 }
