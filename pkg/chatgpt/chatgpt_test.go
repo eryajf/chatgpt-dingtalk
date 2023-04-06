1dd44da9f720c3422c4bea7011855424737fbbe8
river <laolei@forkway.cn>
2023-04-06 16:22:06 +0800

    chore: 调整测试文件

--- pkg/chatgpt/chatgpt_test.go
+++ pkg/chatgpt/chatgpt_test.go
@@ -2,12 +2,10 @@ package chatgpt
 
 import (
 	"fmt"
-	"github.com/eryajf/chatgpt-dingtalk/public"
 	"testing"
 )
 
 func TestChatGPT_ChatWithContext(t *testing.T) {
-	public.InitSvc()
 	chat := New("")
 	defer chat.Close()
 	//go func() {
