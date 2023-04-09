0303a4aed026ad4172c18864541385de3088a91b
Finly <ffinly@gmail.com>
2023-04-09 13:26:27 +0800

    Update main.go

--- main.go
+++ main.go
@@ -57,7 +57,8 @@ func Start() {
 			return nil
 		}
 		// 不在允许群组，不在允许用户（包括在黑名单），不是管理员，任一条件满足，将拒绝进行会话
-		if !public.JudgeGroup(msgObj.GetChatTitle()) && !public.JudgeUsers(msgObj.SenderNick) && !public.JudgeAdminUsers(msgObj.SenderStaffId) {			_, err = msgObj.ReplyToDingtalk(string(dingbot.TEXT), "抱歉，您不在该机器人对话功能的白名单当中！")
+		if !public.JudgeGroup(msgObj.GetChatTitle()) && !public.JudgeUsers(msgObj.SenderNick) && !public.JudgeAdminUsers(msgObj.SenderStaffId) {
+			_, err = msgObj.ReplyToDingtalk(string(dingbot.TEXT), "抱歉，您不在该机器人对话功能的白名单当中！")
 			if err != nil {
 				logger.Warning(fmt.Errorf("send message error: %v", err))
 				return err
