530a60390deba413a1cbee92da97dc02d83fbf63
本光 <121670274+b3nguang@users.noreply.github.com>
2025-02-04 03:59:15 +0800

    Update format.go

--- pkg/chatgpt/format.go
+++ pkg/chatgpt/format.go
@@ -1,12 +1,21 @@
 package chatgpt
 
+import (
+	"regexp"
+	"strings"
+)
+
+// 适配 deepseek r1
 func formatAnswer(answer string) string {
-	for len(answer) > 0 {
-		if answer[:1] == "\n" || answer[0] == ' ' {
-			answer = answer[1:]
-		} else {
-			break
-		}
-	}
+	answer = strings.TrimSpace(answer)
+
+	re := regexp.MustCompile(`(?s)<think>.*?</think>`)
+	answer = re.ReplaceAllString(answer, "")
+
+	answer = strings.ReplaceAll(answer, "<think>", "")
+	answer = strings.ReplaceAll(answer, "</think>", "")
+
+	answer = strings.TrimSpace(answer)
+
 	return answer
 }
